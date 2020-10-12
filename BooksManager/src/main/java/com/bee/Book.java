f7fa4abd8f7da03994cc5dd66427e7d597560e9b
Bawany <bawa.satgu@gmail.com>
2020-10-12 12:48:26 +1000

    added functionality

---
+++ BooksManager/src/main/java/com/bee/Book.java
@@ -0,0 +1,50 @@
+package com.bee;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+	private Integer bookId;
+	private String title;
+	private String author;
+	private float price;
+	
+	@Id
+	@Column(name="book_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Integer getBookId() {
+		return bookId;
+	}
+	public void setBookId(Integer bookId) {
+		this.bookId = bookId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Book [bookId=" + bookId + ", title=" + title + ", author=" + author + ", price=" + price + "]";
+	}
+	
+	
+
+}
